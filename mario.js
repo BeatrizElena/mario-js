2aaeb5f1798d43f94f22df6a1faafb9f4d4e3e02
BeatrizOrtiz <beortiz2010@yahoo.com>
2017-03-09 17:29:05 -0500

    finish validating form submission

--- mario.js
+++ mario.js
@@ -1,34 +1,39 @@
-
+/**Mario4 JS Requirements:
+1. if user doesn't type anything at all, give a different error message, like "Please provide a height"
+2. negative numbers and zero should also be rejected here
+3. implement the clearError(message) function.
+    a. Remove any text from that red label.
+    b. Make sure the input element **does not** have a class of `invalid-field`.
+    c. For reference, you can look at the displayError function. You want to do essentially the opposite of what that function does.
+*/
 
 var heightElem = document.getElementById("height");
 var formElem = document.getElementById("draw-form");
 
 // set a handler function for the form's submission event
 formElem.onsubmit = function(event) {
-
-    // QUIZ
-    // what happens if we don't do this?
+    height = 1;
+    // QUIZ 1: what happens if we don't do this?
     event.preventDefault();
 
-    // QUIZ
-    // what happens if we don't do this?
+    // QUIZ 2: what happens if we don't do this?
     clearError();
 
     // figure out the height the user typed
     heightStr = heightElem.value;
-
-    // TODO 1
-    // if they didn't type anything at all, give a different error message,
-    // something like "Please provide a height"
-
-
+    // TODO 1: error message if user leaves box empty.
+    if(heightStr.length === 0){
+      displayError("Please provide a height");
+      return;
+    }else{
     // convert the string to an int
     height = parseInt(heightStr);
-
+    }
     // if the height is not-a-number, yell at them and exit early
-    // TODO 2
-    // negative numbers and zero should also be rejected here
-    if (isNaN(height)) {
+    // TODO 2: negative numbers and zero get rejected
+    if (height <= 0){
+      displayError("Negative numbers and zero are not valid heights");
+    }else if (isNaN(height)) {
         displayError("That's not a valid height.");
         return;
     }
@@ -42,12 +47,9 @@ formElem.onsubmit = function(event) {
 
     // draw pyramid with the specified height
     drawPyramid(height);
-}
+};
 
-
-/**
- * displayError
- *
+/**displayError
  * Displays an error message on the text input, and colors it red
  */
 function displayError(message) {
@@ -55,18 +57,14 @@ function displayError(message) {
     document.querySelector(".error-message").innerHTML = message;
 }
 
-
-/*
- * clearError
- *
+/*clearError
  * Undisplays the error message and removes the red CSS style
  */
-function clearError(message) {
-    // TODO 3
-    // implement this function.
+function clearError() {
+    // TODO 3: implement this function.
+  heightElem.className = "invalid-field";
+    document.querySelector(".error-message").innerHTML = "";
 }
-
-
 
 /**
  * drawPyramid
@@ -101,3 +99,9 @@ function drawPyramid(height) {
         document.getElementById("pyramid").appendChild(rowElem);
     }
 }
+/** QUIZZES
+1) What happens if we don't do the event.preventDefault();?
+  JS event handlers are usually called before the browser's default behavior is performed. The onsubmit attribute runs by default when a form is submitted. If we don't want the handler to run the onsubmit attribute, we have it call the preventDefault method on the event object.
+2) what happens if we don't have a clearError() fx?
+  Error message stays even if correct height is input. 
+     */
